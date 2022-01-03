8a845456a81c971019ec2afdadde4b469be33e55
Rameshmgr400 <rameshthapamgr400@gmail.com>
2022-01-03 13:25:52 +0545

    string

---
+++ Datatypes/assignment11.py
@@ -0,0 +1,11 @@
+a = "I am a python programmer"
+b = "{1,2}:{3,4}, [1,2,3,4,5], 'Hello'"
+print(a)
+print(a[-1])
+print(a[0])
+print(a[1:6])
+print(a[1:10:2])
+
+c = "Ramesh"
+d = "Magar"
+print("," .join[c, d])
