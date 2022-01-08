b84cac888a48e02d3c425a7b0e1f5cd03327550f
Rameshmgr400 <rameshthapamgr400@gmail.com>
2022-01-08 13:10:16 +0545

    assingment12

--- Datatypes/assignment12.py
+++ Datatypes/assignment12.py
@@ -13,3 +13,24 @@ b = 'r a m e s h magar'
 print(len(a))
 print(len(b))
 
+#Count Vovels
+
+str1 = input("Please Enter Your String : ")
+vowels = 0
+ 
+for i in str1:
+    if(i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'A'
+       or i == 'E' or i == 'I' or i == 'O' or i == 'U'):
+      print("Number of Vowels in this String = ")
+
+#Slipt & Join
+
+a = "I am ramesh magar"
+b = a.split()
+c = a.split('m')
+print(b)
+print(c)
+
+str1 = "Iamrameshmagar"
+a = "-".join(str1)
+print(a)
